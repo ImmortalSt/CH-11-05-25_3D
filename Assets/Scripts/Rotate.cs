5a47fec4967d6e2c84429883bf9376b239b209bc
Anastasia <n_kubko@inbox.ru>
2025-05-11 22:03:45 +1000

    добавлено перемещение вверх и опускание вниз

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -1,16 +1,46 @@
+using UnityEngine;
+
 public class Rotate : MonoBehaviour
-{
+{    
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Start Game");
+        Debug.Log("Start game");
     }
+
+    float targetDistance = 10f; // Общее расстояние, на которое нужно переместить объект
+    float moveIncrement = 0.1f; // Интервал перемещения за кадр
+    float movedDistance = 0f; // Пройденное расстояние
+    bool isMovingUp = true; // Флаг для определения направления движения
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(0, transform.position.y + 0.01f, 0);//текущая позиция
-        transform.Translate(0, 0.1f, 0);//смещение
-        transform.Rotete(0, 90 / 60f, 0);//вращение.
+        if (isMovingUp)
+        {
+            if (movedDistance < targetDistance)
+            {
+                float moveAmount = Mathf.Min(moveIncrement, targetDistance - movedDistance);
+                transform.Translate(0, moveAmount, 0); // Перемещаем объект по оси Y вверх
+                movedDistance += moveAmount; // Увеличиваем пройденное расстояние
+                transform.Rotate(0, 90 / 60f, 0); // Вращение
+            }
+            else
+            {
+                isMovingUp = false; // Меняем направление движения
+            }
+        }
+        else
+        {
+            if (movedDistance > 0)
+            {
+                float moveAmount = Mathf.Min(moveIncrement, movedDistance);
+                transform.Translate(0, -moveAmount, 0); // Перемещаем объект по оси Y вниз
+                movedDistance -= moveAmount; // Уменьшаем пройденное расстояние
+                transform.Rotate(0, 90 / 60f, 0); // Вращение
+            }
+        }
     }
+
+
 }
